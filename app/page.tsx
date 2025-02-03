24b550ce26615e0aae1b96f830985f76b8fb0689
Aubry Cholleton <aubry.cholleton@gmail.com>
2025-02-03 23:55:12 +0100

    basic setup.

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,53 @@
-import Image from "next/image";
-
-export default function Home() {
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-hanken-sans)]">
+      <main className="flex flex-col gap-8 gap-y-4 row-start-2 items-center sm:items-start max-w-screen-md">
+        <h1 className="font-bold text-4xl sm:text-5xl">
+          m<span className="text-accent">i</span>notor
+        </h1>
+        <h2 className="text-xl sm:text-2xl">
+          A lightweight public transit router built for{" "}
+          <span className="text-accent inline-flex flex-col h-[calc(theme(fontSize.xl)*theme(lineHeight.tight))] sm:h-[calc(theme(fontSize.2xl)*theme(lineHeight.tight))] overflow-hidden">
+            <ul className="block animate-text-slide text-left leading-tight [&_li]:block">
+              <li>data viz.</li>
+              <li>research.</li>
+              <li>privacy.</li>
+              <li>the web.</li>
+              <li>mobile apps.</li>
+              <li>data science.</li>
+              <li aria-hidden="true">data viz.</li>
+            </ul>
+          </span>
+        </h2>
+        <section className="text-justify space-y-4">
+          <p>
+            Unlike most transit planners out there, minotor can store all the
+            transit data for a given day in memory on the client, allowing for
+            fast runtime queries using only local data.{" "}
+          </p>
+          <p>
+            This is particularly useful for highly dynamic applications or
+            complex visualizations for research purposes where the user needs to
+            query the data in real-time. Privacy-conscious applications where
+            the user does not want to share their location data with a server
+            can also benefit from this model.
+          </p>
+          <p>
+            The transit router and the stops index of minotor can run in the
+            browser, on react-native or in a Node.js environment. Transit data
+            (GTFS) parsing runs on Node.js, and the resulting data is serialized
+            as a protobuf binary that can be loaded from the router.
+          </p>
+        </section>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Get started
           </a>
         </div>
       </main>
@@ -56,14 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          GitHub
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -71,31 +66,11 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Author
         </a>
       </footer>
     </div>
   );
-}
+};
+
+export default Home;
